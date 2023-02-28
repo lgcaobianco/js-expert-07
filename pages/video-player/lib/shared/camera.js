2bb7c2f1a92ceb7f3091846f03ba15e8846e2f45
Luiz G. Caobianco <gu.caobianco@gmail.com>
2023-02-28 19:17:16 -0300

    added camera access

--- pages/video-player/lib/shared/camera.js
+++
@@ -1,5 +0,0 @@
-export default class Camera {
-  static async init() {
-
-  }
-}
--- pages/video-player/src/factory.js
+++ pages/video-player/src/factory.js
@@ -1,7 +1,9 @@
+import Camera from '../../../lib/shared/camera.js';
 import Controller from './controller.js';
 import Service from './service.js';
 import View from './view.js';
 
+const camera = await Camera.init();
 const [rootPath] = window.location.href.split('/pages/');
 const factory = {
   async initalize() {
