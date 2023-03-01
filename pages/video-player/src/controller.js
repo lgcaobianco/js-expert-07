8884b872d38e092607fdf62894550ad570a91984
Luiz G. Caobianco <gu.caobianco@gmail.com>
2023-02-28 21:39:11 -0300

    finished class 1

--- pages/video-player/src/controller.js
+++ pages/video-player/src/controller.js
@@ -1,9 +1,14 @@
+
 export default class Controller {
     #view;
+    #worker;
+    #camera;
     #service;
-    constructor({ view, service }) {
+    #blinkCounter;
+    constructor({ view, worker, camera }) {
       this.#view = view
-      this.#service = service
+      this.#camera = camera
+      this.#worker = this.#configureWorker(worker);
       this.#view.configureOnBtnClick(this.onBtnStart.bind(this));
     }
 
@@ -13,15 +18,50 @@ export default class Controller {
     return controller.init();
   }
 
+  #configureWorker(worker){
+    let ready=false;
+    worker.onmessage = ({data}) => {
+      console.log(data);
+      if(data === 'READY'){
+        this.#view.enableButton();
+        ready  = true;
+        return;
+      }
+      const blinked = data.blinked;
+      this.#blinkCounter += blinked;
+      this.#view.togglePlayVideo();
+      console.log('blinked', blinked);
+    }
+    
+    return {
+      send (msg) {
+        if(!ready) return;
+        worker.postMessage(msg)
+      }
+    }
+  }
+  loop() {
+    const video = this.#camera.video;
+    const img = this.#view.getVideoFrame(video);
+    this.#worker.send(img);
+    this.log('detecting eye blink')
+    setTimeout(() => this.loop(), 100)
+
+  }
+
   async init() {
     console.log('init!!!');
   }
 
   log(text) {
-    this.#view.log(text);
+    const times = `        = blinked times  ${this.#blinkCounter}`
+    this.#view.log(`status: ${text}`.concat(this.#blinkCounter ? times : ""));
   }
 
   onBtnStart() {
+    this.#blinkCounter = 0;
     this.log('started detection')
+    this.loop();
   }
+
 }
